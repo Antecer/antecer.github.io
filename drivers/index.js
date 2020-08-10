950fad5a920c061da7bc4b8506b0f531485cbb90
Antecer <antecer@outlook.com>
2020-08-10 17:55:01 +0800

    调整Drivers页面事件绑定方式.

--- drivers/index.js
+++ drivers/index.js
@@ -1,16 +1,29 @@
-function downloadApp() {
-	fetch('/drivers/app/AppVerList.md')
-		.then(res => res.text())
-		.then(txt => {
-			let versions = txt.split('\n');
-			console.log('AppVerList:', versions);
-			let lastVer = versions.length > 0 ? versions[0] : '0.0.0.0';
+(() => {
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
 
-			let downnode = document.createElement("a");
-			downnode.download = 'AmusingDeviceApplication.exe';
-			downnode.href = `/drivers/app/app_${lastVer}`;
-			document.body.appendChild(downnode); // Fix for firefox, the anchor has to be appended to the DOM.
-			downnode.click();
-			document.body.removeChild(downnode);
-		})
-}
+	(async () => {
+		while (true) {
+			if (document.querySelector('.downpanel>input')) break;
+			await sleep(1000);
+		}
+
+		document.querySelector('.downpanel>input').addEventListener('click', function () {
+			fetch('/drivers/app/AppVerList.md')
+				.then(res => res.text())
+				.then(txt => {
+					let versions = txt.split('\n');
+					console.log('AppVerList:', versions);
+					let lastVer = versions.length > 0 ? versions[0] : '0.0.0.0';
+
+					let downnode = document.createElement("a");
+					downnode.download = 'AmusingDeviceApplication.exe';
+					downnode.href = `/drivers/app/app_${lastVer}`;
+					document.body.appendChild(downnode); // Fix for firefox, the anchor has to be appended to the DOM.
+					downnode.click();
+					document.body.removeChild(downnode);
+				});
+		});
+	})();
+})();
