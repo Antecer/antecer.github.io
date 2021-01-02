388e1f0948666bbb4fa23dfa6ca88cee2abe69b0
Antecer <antecer@outlook.com>
2021-01-02 11:50:24 +0800

    优化配置程序下载体验

--- drivers/index.js
+++ drivers/index.js
@@ -9,21 +9,28 @@
 			await sleep(1000);
 		}
 
-		document.querySelector('.downpanel>input').addEventListener('click', function () {
-			fetch('/drivers/app/AppVerList.md')
+		document.querySelector('.downpanel>input').addEventListener('click', async () => {
+			let lastVer = await fetch('/drivers/app/AppVerList.md')
 				.then(res => res.text())
 				.then(txt => {
 					let versions = txt.split('\n');
 					console.log('AppVerList:', versions);
-					let lastVer = versions.length > 0 ? versions[0] : '0.0.0.0';
-
-					let downnode = document.createElement("a");
-					downnode.download = 'AmusingDeviceApplication.exe';
-					downnode.href = `/drivers/app/app_${lastVer}`;
-					document.body.appendChild(downnode); // Fix for firefox, the anchor has to be appended to the DOM.
-					downnode.click();
-					document.body.removeChild(downnode);
+					return versions.length > 0 ? versions[0] : '0.0.0.0';
 				});
+			if (lastVer != '0.0.0.0') {
+				fetch(`/drivers/app/app_${lastVer}`, { responseType: 'blob' })
+					.then((res) => res.blob())
+					.then((blob) => {
+						let link = document.createElement("a");
+						link.download = 'AmusingDeviceApplication.exe';
+						link.href = window.URL.createObjectURL(blob);
+						document.body.appendChild(link); // Fix for firefox, the anchor has to be appended to the DOM.
+						link.click();
+						window.URL.revokeObjectURL(link.href);
+						document.body.removeChild(link);
+					});
+			}
+			else console.log('Download Failed!');
 		});
 	})();
 })();
