ee9169781d3e364fc290313e10b9ec3ccd379d77
Antecer <antecer@outlook.com>
2021-02-07 11:25:51 +0800

    Try to fix the bug that app download is recognized as txt

--- drivers/index.js
+++ drivers/index.js
@@ -1,6 +1,6 @@
 (() => {
 	function sleep(ms) {
-		return new Promise(resolve => setTimeout(resolve, ms));
+		return new Promise((resolve) => setTimeout(resolve, ms));
 	}
 
 	(async () => {
@@ -10,27 +10,34 @@
 		}
 
 		document.querySelector('.downpanel>input').addEventListener('click', async () => {
-			let lastVer = await fetch('/drivers/app/AppVerList.md')
-				.then(res => res.text())
-				.then(txt => {
-					let versions = txt.split('\n');
-					console.log('AppVerList:', versions);
-					return versions.length > 0 ? versions[0] : '0.0.0.0';
-				});
-			if (lastVer != '0.0.0.0') {
-				fetch(`/drivers/app/app_${lastVer}`, { responseType: 'blob' })
-					.then((res) => res.blob())
-					.then((blob) => {
-						let link = document.createElement("a");
-						link.download = 'AmusingDeviceApplication.exe';
-						link.href = window.URL.createObjectURL(blob);
-						document.body.appendChild(link); // Fix for firefox, the anchor has to be appended to the DOM.
-						link.click();
-						window.URL.revokeObjectURL(link.href);
-						document.body.removeChild(link);
-					});
+			try {
+				let verResponse = await fetch('/drivers/app/AppVerList.md');
+				let txt = await verResponse.text();
+				let versions = txt.split('\n');
+				console.log('AppVerList:', versions);
+				let lastVer = versions.length > 0 ? versions[0] : '0.0.0.0';
+				if (lastVer == '0.0.0.0') {
+					console.log('Download Failed!');
+					return;
+				}
+
+				let appResponse = await fetch(`/drivers/app/app_${lastVer}`, { responseType: 'blob' });
+				let blob = await appResponse.blob();
+				let reader = new FileReader();
+				reader.onload = function () {
+					let appBlob = new Blob([this.result], { type: 'application/vnd.microsoft.portable-executable' });
+					let link = document.createElement('a');
+					link.download = 'AmusingDeviceApplication.exe';
+					link.href = window.URL.createObjectURL(appBlob);
+					document.body.appendChild(link);
+					link.click();
+					window.URL.revokeObjectURL(link.href);
+					document.body.removeChild(link);
+				};
+				reader.readAsArrayBuffer(blob);
+			} catch (err) {
+				console.error(err);
 			}
-			else console.log('Download Failed!');
 		});
 	})();
 })();
